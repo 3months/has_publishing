5995002fa336a86a47dadd068be3dcf9dcb523d3
Josh McArthur <joshua.mcarthur@gmail.com>
2012-12-14 20:38:54 +1300

    Set default configuration settings

--- lib/has_publishing/configuration.rb
+++ lib/has_publishing/configuration.rb
@@ -1,3 +1,7 @@
 module HasPublishing
   include ActiveSupport::Configurable
+
+  # Set some default config
+  self.config.scope_records = true
+  self.config.published_rails_environment = "production"
 end
