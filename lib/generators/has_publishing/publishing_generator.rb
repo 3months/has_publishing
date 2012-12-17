ecfc5f4ffb56d8c77f669161b536f7dab4576e98
Josh McArthur <joshua.mcarthur@gmail.com>
2012-12-17 14:03:05 +1300

    Properly namespace generator

--- lib/generators/has_publishing/publishing_generator.rb
+++ lib/generators/has_publishing/publishing_generator.rb
@@ -1,28 +1,30 @@
 require 'rails/generators'
 require 'rails/generators/migration'
 
-class HasPublishing::PublishingGenerator < ::Rails::Generators::Base
-  include Rails::Generators::Migration
+module HasPublishing
+  class PublishingGenerator < ::Rails::Generators::Base
+    include Rails::Generators::Migration
 
-  def self.source_root
-    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-  end
-
-  def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      Time.new.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
     end
-  end
 
-  def create_migration_file
-    set_local_assigns!
-    validate_file_name!
-    migration_template 'migration.rb', "db/migrate/publishing_fields_for_#{@table_name}.rb"
-  end
+    def self.next_migration_number(dirname)
+      if ActiveRecord::Base.timestamped_migrations
+        Time.new.utc.strftime("%Y%m%d%H%M%S")
+      else
+        "%.3d" % (current_migration_number(dirname) + 1)
+      end
+    end
 
-  def set_local_assigns!
-    @table_name = file_name.pluralize
+    def create_migration_file
+      set_local_assigns!
+      validate_file_name!
+      migration_template 'migration.rb', "db/migrate/publishing_fields_for_#{@table_name}.rb"
+    end
+
+    def set_local_assigns!
+      @table_name = file_name.pluralize
+    end
   end
 end
